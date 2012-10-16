955650b49c87ed48267ea93890426cc79b887dc1
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2012-10-15 20:22:01 -0700

    sign.sh now creates slices only if requested from command line (-s option)

--- sign.sh
+++ sign.sh
@@ -10,6 +10,29 @@ SIGNING_KEY_NAME=/ndn/keys/ucla.edu
 KEY_PREFIX=/ndn/keys/ucla.edu
 CERTS=$D/certs
 
+SYNC_TOPO_PREFIX="/ndn/broadcast/sync/keys"
+SYNC_NAME_PREFIX="/ndn/keys"
+
+while getopts "hs" flag; do
+    case "$flag" in
+	s) 
+            echo Creating repo slice
+            ccnsyncslice create "$SYNC_TOPO_PREFIX" "$SYNC_NAME_PREFIX"
+            RET=$?
+            echo "Status of ccnsyncslice create: $RET"
+            ;;
+        h | ?)
+            cat <<EOF
+    $0 [-h] [-s]
+	Sign user public keys
+
+	-h print this help message
+	-s create sync slice
+EOF
+            exit 1
+    esac
+done
+
 for cert in ${CERTS}/*
 do
   USER=`basename $cert .pem`
