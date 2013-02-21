6b014faf300017e5f112cf286eaf9808ea1d5008
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2013-02-21 14:29:46 -0800

    An extended version of the signing script, asking for the real-world identity for each key

--- sign.sh
+++ sign.sh
@@ -4,7 +4,7 @@ D=`dirname "$0"`
 
 MKEY=$D/mkey/ccn-publish-key.sh
 KEYSTORE=$D/site-keystore/
-AFFI=${AFFI:-"UCLA"}
+AFFI=${AFFI:-"University of California, Los Angeles"}
 VALID_DAYS=365
 SIGNING_KEY_NAME=${SIGNING_KEY_NAME:-"/ndn/keys/ucla.edu"}
 KEY_PREFIX=${KEY_PREFIX:-"/ndn/keys/ucla.edu"}
@@ -15,7 +15,7 @@ SYNC_NAME_PREFIX="/ndn/keys"
 
 function usage {
             cat <<EOF
-Usage:  
+Usage:
   $0 [-h] (-s | -S)
       Sign user public keys
 
@@ -28,7 +28,7 @@ EOF
 
 while getopts "hsS" flag; do
     case "$flag" in
-	s) 
+	s)
             echo Creating repo slice
             ccnsyncslice create "$SYNC_TOPO_PREFIX" "$SYNC_NAME_PREFIX"
             RET=$?
@@ -49,18 +49,37 @@ EOF
 	    while true; do
 		read -p "Sign and publish user public keys with above parameters (yes|no)? " yn
 		case $yn in
-		    [Yy][eE][sS] ) 
+		    [Yy][eE][sS] )
 			for cert in `ls ${CERTS}/*.pem 2>/dev/null`; do
-			    USER=`basename $cert .pem`
-			    $MKEY -i $USER -a $AFFI -f $cert -F $KEYSTORE -P $SIGNING_KEY_NAME -p ${KEY_PREFIX}/${USER} -x $VALID_DAYS && echo "Signed $USER"
+                            USER=`basename $cert .pem`
+
+                            read -p "Sign $cert (yes|no) [yes]: " yn
+                            if [ "x$yn" == "xno" -o "x$yn" == "NO" -o "x$yn" == "nO" -o "x$yn" == "No" ]; then
+                                continue
+                            fi
+
+                            read -p "Enter key name [${KEY_PREFIX}/${USER}]: " keyname
+                            if [ "x$keyname" == "x" ]; then
+                                keyname=${KEY_PREFIX}/${USER}
+                            fi
+
+                            while true; do
+                                read -p "Enter real world identity of the signed key (full name): " real_identity
+                                if [ "x$real_identity" == "x" ]; then
+                                    echo "ERROR: you must specify real world identity for the signed key"
+                                else
+			            $MKEY -i "$real_identity" -a "$AFFI" -f "$cert" -F "$KEYSTORE" -P "$SIGNING_KEY_NAME" -p "$keyname" -x "$VALID_DAYS" && echo "Signed $USER"
+                                    break
+                                fi
+                            done
 			done
 			exit 0
 
 			;;
-		    [Nn][oO] ) 
+		    [Nn][oO] )
 			exit 1
 			;;
-		    * ) 
+		    * )
 			echo "Please answer yes or no."
 			;;
 		esac
